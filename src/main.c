a143c34ff8bca82d7e1eafe0811fa01bc275bb66
Remco van Zuijlen <remcovz@users.noreply.github.com>
2014-12-09 17:44:45 +0100

    current state
    Does give build errors though

--- src/main.c
+++ src/main.c
@@ -2,7 +2,9 @@
   
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static TextLayer *s_weather_layer;
 static GFont s_time_font;
+static GFont s_weather_font;
 static BitmapLayer *s_background_layer;
 static GBitmap *s_background_bitmap;
 
@@ -57,17 +59,32 @@ static void main_window_load(Window *window) {
 
   // Add it as a child layer to the Window's root layer
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+  
+  // Create temperature Layer
+  s_weather_layer = text_layer_create(GRect(0, 130, 144, 25));
+  text_layer_set_background_color(s_weather_layer, GColorClear);
+  text_layer_set_text_color(s_weather_layer, GColorWhite);
+  text_layer_set_text_alignment(s_weather_layer, GTextAlignmentCenter);
+  text_layer_set_text(s_weather_layer, "Loading...");
+  
+  // Create second custom font, apply it and add to Window
+  s_weather_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_24));
+  text_layer_set_font(s_weather_layer, s_weather_font);
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_weather_layer));
 }
 
 static void main_window_unload(Window *window) {
-    // Destroy TextLayer
-    text_layer_destroy(s_time_layer);
-    // Unload GFont
-    fonts_unload_custom_font(s_time_font);
-    // Destroy GBitmap
-    gbitmap_destroy(s_background_bitmap);
-    // Destroy BitmapLayer
-    bitmap_layer_destroy(s_background_layer);
+  // Destroy TextLayer
+  text_layer_destroy(s_time_layer);
+  // Unload GFont
+  fonts_unload_custom_font(s_time_font);
+  // Destroy weather elements
+  text_layer_destroy(s_weather_layer);
+  fonts_unload_custom_font(s_weather_font);
+  // Destroy GBitmap
+  gbitmap_destroy(s_background_bitmap);
+  // Destroy BitmapLayer
+  bitmap_layer_destroy(s_background_layer);
 }
 
 static void init() {
